f66a0fb03c9225189e95b682f2f2b8191bd46c25
Mike Bild <mike@mikebild.com>
2022-07-31 18:45:30 +0200

    add PWA

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,6 +5,17 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<link rel="manifest" href="/manifest.webmanifest" />
+	<link rel="apple-touch-icon" href="/logo_512.png" />
+
+	<meta name="Cloud Driven" content="Cloud Driven" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<link rel="apple-touch-icon" href="/logo_192.png" />
+	<meta name="msapplication-TileColor" content="#00aba9" />
+	<meta name="theme-color" content="#bee0da" />
+</svelte:head>
+
 <Header />
 
 <slot />
